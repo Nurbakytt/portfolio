0eaab57c0eabff4f3c227a2c34a54dfe3b997cad
Nurbakytt <nurbakytdl@gmail.com>
2020-11-26 10:56:48 +0600

    create footer, added icons, change main.js code

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
-let menuToggle = document.querySelector('#menu_toggle')
-let menu = document.querySelector('.menu')
-menuToggle.addEventListener('click',function(){
-    
-}) 
+// const menuToggle = document.querySelector('#menu_toggle')
+// const menuContent = document.querySelector('.menu-content')
+
+
+// menuToggle.addEventListener('click', function() {
+//     menuContent.classList.toggle('active')
+//     menuToggle.classList.toggle('active')
+// })
+
+const navbar = document.querySelector('.navbar')
+const ham = document.querySelector('.ham')
+
+function toggleHamburger() {
+    navbar.classList.toggle('showNav')
+    ham.classList.toggle('showClose')
+}
+
+ham.addEventListener('click', toggleHamburger)
+
+
+
+
+
+
